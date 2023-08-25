6107ef7174851bdee1104a2b104e8ba47f217750
tavberidze <nikoloz.tavberidze@geolab.edu.ge>
2023-08-25 20:44:17 +0400

    assignment

---
+++ foundations/js-basics/assignments/fundamentals-part1/script.js
@@ -0,0 +1,10 @@
+console.log(23 + 97);
+console.log(5 + 73 + 66 + 31 + 8 + 59);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual/max;
